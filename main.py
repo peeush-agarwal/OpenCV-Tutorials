b975905178d75be962ee058c8b42a785926dfc6a
Peeush Agarwal <agarwal.peeush@gmail.com>
2020-05-22 23:07:43 +0530

    Detect facial landmarks from webcam

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import argparse
 from video_processor import read_video
 from colored_object_tracking import track_objects, HSV_Limits
 from thresholding import apply_thresholding
+from facial_landmarks import detect_face
 
 ap = argparse.ArgumentParser()
 ap.add_argument('-wc', '--web_cam', default=False, action='store_true', help="Use Integrated webcam")
@@ -12,7 +13,7 @@ args = ap.parse_args()
 if __name__ == "__main__":
     if args.web_cam:
         print('Processing video from webcam')
-        cap = cv2.VideoCapture(0)
+        cap = cv2.VideoCapture(0 + cv2.CAP_DSHOW) # (0) not working.
     elif args.url:
         print(f'Processing video from {args.url}')
         # url='http://192.168.1.40:8080/video'
@@ -22,15 +23,30 @@ if __name__ == "__main__":
         exit(0)
 
     # read_video(cap)
-
-    color_limits = list()
-    color_limits.append(HSV_Limits())
+    
+    # color_limits = list()
+    # color_limits.append(HSV_Limits())
     # color_limits.append(HSV_Limits(color='green'))
     # color_limits.append(HSV_Limits(color='red'))
 
     # track_objects(cap, color_limits, display_masks=True)
 
-    apply_thresholding(cap)
+    # apply_thresholding(cap)
+
+    # print(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    # print(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    while cap.isOpened():
+        ret, frame = cap.read()
+        # print(ret)
+
+        if frame is None:
+            break
+
+        img = detect_face(frame)
+        
+        cv2.imshow('Processed', img)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
     cap.release()
     cv2.destroyAllWindows()
