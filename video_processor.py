f89a94d73fbd61a75062ba5d274aa024b34f00aa
Peeush Agarwal <agarwal.peeush@gmail.com>
2020-05-23 12:49:26 +0530

    Add video_facial_landmarks method to detect facial_landmarks in real time

--- video_processor.py
+++ video_processor.py
@@ -1,5 +1,8 @@
 import cv2
 import numpy as np
+import dlib
+import imutils
+from imutils import face_utils
 
 def image_processing(img):
     # _, imgProc = cv2.threshold(img, 60, 255, cv2.THRESH_BINARY)
@@ -31,3 +34,32 @@ def read_video(cap):
         # Quit if q is pressed
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
+
+def video_facial_landmarks(cap, shape_predictor_path = 'Data\shape_predictor_68_face_landmarks.dat'):
+    detector = dlib.get_frontal_face_detector()
+    predictor = dlib.shape_predictor(shape_predictor_path)
+    while cap.isOpened():
+        ret, frame = cap.read()
+
+        if frame is None:
+            break
+        
+        frame = imutils.resize(frame, width=400)
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        rects = detector(gray, 1)
+
+        for i, rect in enumerate(rects):
+            shape = predictor(gray, rect)
+            shape = face_utils.shape_to_np(shape)
+
+            for (x,y) in shape:
+                cv2.circle(frame, (x,y), 1, (0,0,255),-1)
+        
+        cv2.imshow("Frame", frame)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    
+    cap.release()
+    cv2.destroyAllWindows()
